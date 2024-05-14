1bc0bdafe5b8e46271b7d66d672177145a9ed660
xinchengli1112 <141010965+xinchengli1112@users.noreply.github.com>
2024-05-14 15:50:37 +0800

    Update forms.py
    add class Registration Form to forms.py

--- app/forms.py
+++ app/forms.py
@@ -18,7 +18,23 @@ class StaffLoginForm(FlaskForm):
 
 
 class RegistrationForm(FlaskForm):
-    pass
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = UserModel.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = UserModel.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+        
 
 class QuestionForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
