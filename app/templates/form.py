3be0a6362ebac26feb540540185e88a4f29bdaae
953740669wu <168261138+953740669wu@users.noreply.github.com>
2024-05-14 09:25:56 +0800

    Delete form.py

--- app/templates/form.py
+++
@@ -1,5 +0,0 @@
-import wtforms
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, IntegerField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length, InputRequired
-from app.models import UserModel
