db76c54c56423b0f24fb5a3af15d97959c184c81
953740669wu <168261138+953740669wu@users.noreply.github.com>
2024-05-14 09:04:55 +0800

    Update __init__.py
    improve the __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,24 @@
-from flask import Flask
+import sys
+import os
+from flask import Flask, session, g
+from app.config import Config
+from app.extensions import db, migrate, login_manager
+from app.models import UserModel
+from flask_login import LoginManager
 
 app = Flask(__name__, template_folder='templates')
+app.config.from_object(Config)
+
+
+db.init_app(app)
+migrate.init_app(app, db)
+login_manager.init_app(app)  
+
+from app import routes
+
+
 
 if __name__ == '__main__':
+    for rule in app.url_map.iter_rules():
+        print(rule)
     app.run(debug=True)
