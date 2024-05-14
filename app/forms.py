ed84e9e940f39758d405d30dd81d61aadde193f3
953740669wu <168261138+953740669wu@users.noreply.github.com>
2024-05-14 09:50:01 +0800

    update forms and routes
    Update some definitions in forms to ensure that routes can be successfully imported

--- app/forms.py
+++ app/forms.py
@@ -3,3 +3,17 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, IntegerField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length, InputRequired
 from app.models import UserModel
+
+class LoginForm(FlaskForm):
+    pass
+
+class StaffLoginForm(FlaskForm):
+    pass
+
+class RegistrationForm(FlaskForm):
+    pass
+class QuestionForm(FlaskForm):
+    pass
+
+class AnswerForm(wtforms.Form):
+    pass
