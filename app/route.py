deaaeca6c215df6ae8fc1ce447bcd728d77477b0
953740669wu <168261138+953740669wu@users.noreply.github.com>
2024-05-14 09:03:07 +0800

    Create route.py
    Create a route and import the packages that might be used

---
+++ app/route.py
@@ -0,0 +1,10 @@
+from flask import render_template, request, redirect, url_for, flash
+from app import app, db
+from app.models import UserModel, QuestionModel, AnswerModel
+from flask_mail import Message
+from flask_login import logout_user
+from flask_login import login_required
+from flask_login import current_user
+from flask_login import login_user
+from app.forms import RegistrationForm, QuestionForm, AnswerForm
+from sqlalchemy import select
