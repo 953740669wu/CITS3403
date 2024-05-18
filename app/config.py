292d56db68f98a8fe812db8cb66fa45bc448c1bc
xinchengli1112 <141010965+xinchengli1112@users.noreply.github.com>
2024-05-18 20:42:41 +0800

    email function

--- app/config.py
+++ app/config.py
@@ -8,6 +8,22 @@ class Config:
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    # set the email system
+    MAIL_DEBUG = True
+    MAIL_SERVER = "smtp.googlemail.com"
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = "lixincheng57@gmail.com"
+    MAIL_PASSWORD = "lixingcheng123"
+    FLASKY_MAIL_SUBJECT_PREFIX = "[Flasky]"
+    FLASKY_MAIL_SENDER = "lixincheng57@gmail.com"
+    FLASKY_ADMIN = "lixincheng57@gmail.com"
+    ADMINS = ["lixincheng57@gmail.com"]  # Add ADMINS here
+
+    
+   
+
 class DevelopmentConfig(Config):
     DEBUG = True
 
